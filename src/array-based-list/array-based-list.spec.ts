7405f9b724739dcd05db1037e883bbe05d38adf5
IYuriev <ilya.yuriev.s@gmail.com>
2025-03-20 17:25:22 +0200

    Add unit tests for append & get methods

--- src/array-based-list/array-based-list.spec.ts
+++ src/array-based-list/array-based-list.spec.ts
@@ -12,4 +12,20 @@ describe("ArrayBasedList", () => {
       expect(list.length()).toBe(0);
     });
   });
+
+  describe("append", () => {
+    it("should append elements and increase length", () => {
+      list.append("A");
+      list.append("B");
+      expect(list.length()).toBe(2);
+      expect(list.get(0)).toBe("A");
+      expect(list.get(1)).toBe("B");
+    });
+
+    it("should append an element to an empty list", () => {
+      list.append("X");
+      expect(list.length()).toBe(1);
+      expect(list.get(0)).toBe("X");
+    });
+  });
 });
