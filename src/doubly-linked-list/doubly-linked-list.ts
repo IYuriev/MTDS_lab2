8c5382dccb7ff929f98c4e85df03adff312c6cb5
IYuriev <ilya.yuriev.s@gmail.com>
2025-03-20 21:04:43 +0200

    Add insert & delete & get methods

--- src/doubly-linked-list/doubly-linked-list.ts
+++ src/doubly-linked-list/doubly-linked-list.ts
@@ -42,4 +42,57 @@ export class DoublyLinkedList {
     this.tail = null;
     this.size = 0;
   }
+
+  insert(element: string, index: number): void {
+    if (index < 0 || index > this.size) throw new Error("Index out of bounds");
+    const newNode = new Node(element);
+    if (index === 0) {
+      newNode.next = this.head;
+      if (this.head) this.head.prev = newNode;
+      this.head = newNode;
+      if (!this.tail) this.tail = newNode;
+    } else {
+      let current = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        current = current!.next;
+      }
+      newNode.next = current!.next;
+      newNode.prev = current;
+      if (current!.next) current!.next.prev = newNode;
+      current!.next = newNode;
+      if (!newNode.next) this.tail = newNode;
+    }
+    this.size++;
+  }
+
+  delete(index: number): string {
+    if (index < 0 || index >= this.size) throw new Error("Index out of bounds");
+    let deletedValue: string;
+    if (index === 0) {
+      deletedValue = this.head!.value;
+      this.head = this.head!.next;
+      if (this.head) this.head.prev = null;
+      else this.tail = null;
+    } else {
+      let current = this.head;
+      for (let i = 0; i < index; i++) {
+        current = current!.next;
+      }
+      deletedValue = current!.value;
+      if (current!.prev) current!.prev.next = current!.next;
+      if (current!.next) current!.next.prev = current!.prev;
+      if (current === this.tail) this.tail = current!.prev;
+    }
+    this.size--;
+    return deletedValue;
+  }
+
+  get(index: number): string {
+    if (index < 0 || index >= this.size) throw new Error("Index out of bounds");
+    let current = this.head;
+    for (let i = 0; i < index; i++) {
+      current = current!.next;
+    }
+    return current!.value;
+  }
 }
